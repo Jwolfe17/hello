0632cc81f1e06e7aafe8d3b5c12bc9a94b59d592
oliviermizero <59984459+oliviermizero@users.noreply.github.com>
2020-01-16 19:03:02 -0600

    Update hello.c

--- hello/hello.c
+++ hello/hello.c
@@ -1,11 +1,14 @@
 /*"Hello World", My first program
 Name: Jenna Wolfe
+Collaborator: Olivier Niyonshuti Mizero
 Date: 2020/14/01
 */
+
 #include <stdio.h>
 
 int main(void) {
 	printf("Jenna Wolfe undecided\n");
+	printf("Olivier Niyonshuti Mizero, Integrated Science\n");
 	return 0;
 }
 
